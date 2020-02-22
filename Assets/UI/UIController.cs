d5886571915ffe8f53c492ed1942f060ef060370
alexblouin <alex.blouin64@gmail.com>
2020-02-22 13:02:25 -0500

    Added UIController and updated UI prefab

---
+++ Assets/UI/UIController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject menu;
+    [SerializeField]
+    private GameObject gameUI;
+    [SerializeField]
+    private GameObject gameEnd;
+
+    public void showMenu(bool show)
+    {
+        // Can replace for anything else (like animation or something)
+        menu.SetActive(show);
+    }
+    public void showGameUI(bool show)
+    {
+        gameUI.SetActive(show);
+    }
+
+    public void showGameEnd(bool show)
+    {
+        gameEnd.SetActive(show);
+    }
+}
