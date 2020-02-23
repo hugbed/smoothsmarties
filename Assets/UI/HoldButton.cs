6cd7731fa16926c6766245cfc14ef12e8cd7767e
alexblouin <alex.blouin64@gmail.com>
2020-02-22 21:20:12 -0500

    Added Button holding

---
+++ Assets/UI/HoldButton.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+[System.Serializable]
+public class OnButtonHold : UnityEvent{}
+
+public class HoldButton : MonoBehaviour
+{
+    public double holdTime = 0.5;
+    private double time = 0;
+
+    private bool holding = false;
+
+    public OnButtonHold holdEvent;
+
+    // Start is called before the first frame update
+
+    // Update is called once per frame
+
+    public void beginHold()
+    {
+        time = 0.0;
+        holding = true;
+    }
+
+    public void endHold()
+    {
+        holding = false;
+        time = 0.0;
+    }
+
+    void Update()
+    {
+        if (holding)
+        {
+            time += Time.deltaTime;
+        }
+        if (holding && time >= holdTime)
+        {
+            holdEvent.Invoke();
+            time = 0.0;
+            holding = false;
+        }
+    }
+}
