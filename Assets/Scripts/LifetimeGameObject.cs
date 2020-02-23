004facf6e5ea85318e5209ff9e3fe889683a0842
MarcoPolo <mannad1118@gmail.com>
2020-02-23 11:37:18 -0500

    New electrified effect

---
+++ Assets/Scripts/LifetimeGameObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifetimeGameObject : MonoBehaviour
+{
+    public float lifeTime;
+    private float elapsedTime = 0.0f;
+
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = this.GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+
+        float alpha = (lifeTime - elapsedTime) / lifeTime;
+        spriteRenderer.color = new Color(1f, 1f, 1f, alpha);
+
+        if (elapsedTime >= lifeTime)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
